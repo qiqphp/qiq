793993a3506536a647937dab6d0bdc3932ea383e
Paul M. Jones <pmjones@pmjones.io>
2023-04-05 10:25:15 -0500

    add test for functions as helpers

---
+++ tests/HelpersTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Qiq;
+
+use Qiq\Exception;
+use Qiq\Helper\Html\FakeBroken;
+use Qiq\Helper\Html\FakeHello;
+
+class HelpersTest extends \PHPUnit\Framework\TestCase
+{
+    public function test__call() : void
+    {
+        $helpers = new Helpers();
+        $expect = 'FOOBAR';
+        /** @phpstan-ignore-next-line checking function fallback for missing method */
+        $actual = $helpers->strtoupper('foobar');
+        $this->assertSame($expect, $actual);
+    }
+}
