cd159653c4e68fbad5ff3bf1b8e53eb256265e26
Paul M. Jones <pmjones@pmjones.io>
2023-09-13 10:48:07 -0500

    add styler config

---
+++ php-styler.php
@@ -0,0 +1,11 @@
+<?php
+use PhpStyler\Config;
+use PhpStyler\Files;
+use PhpStyler\Styler;
+
+return new Config(
+    files: new Files(
+        __DIR__ . '/src',
+    ),
+    styler: new Styler(),
+);
